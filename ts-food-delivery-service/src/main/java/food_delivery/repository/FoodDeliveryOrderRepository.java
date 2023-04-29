eb1acce007c74de8cb8228c463eb251847eb6df9
xyc0209 <xingyongchao1997@126.com>
2023-04-29 23:15:14 +0800

    first commit

---
+++ ts-food-delivery-service/src/main/java/food_delivery/repository/FoodDeliveryOrderRepository.java
@@ -0,0 +1,17 @@
+package food_delivery.repository;
+
+import com.septemberhx.mclient.annotation.Loggable;
+import food_delivery.entity.FoodDeliveryOrder;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface FoodDeliveryOrderRepository extends CrudRepository<FoodDeliveryOrder, String> {
+    @Loggable
+    List<FoodDeliveryOrder> findByStationFoodStoreId(String stationFoodStoreId);
+
+    @Override
+    @Loggable
+    List<FoodDeliveryOrder> findAll();
+
+}
